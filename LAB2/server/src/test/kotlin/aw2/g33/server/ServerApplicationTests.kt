241201a68bf0737e03c759665ebb0d97dec47e05
s292567 <101062653+s292567@users.noreply.github.com>
2023-03-30 18:15:22 +0200

    feat: added second lab's spring boot server module

---
+++ LAB2/server/src/test/kotlin/aw2/g33/server/ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package aw2.g33.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# AW2
+# AW2 - LABS
+Repository containing all labs of G33 team for course WA2-2023
+
+Members of this team are:
+* Nicolò Fontana
+* Matteo Fontana
+* Membro 3
+* Membro 4
  
