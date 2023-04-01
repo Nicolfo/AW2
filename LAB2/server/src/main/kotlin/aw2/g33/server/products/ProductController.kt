aba8bd3bf7a74d7ea12421c425a2f9860d20f153
s292567 <101062653+s292567@users.noreply.github.com>
2023-04-01 13:42:10 +0200

    feat: added @ResponseStatus for all the Controllers TODO: check if error message are implemented correctly, and that all possible error are detected and managed correctly

--- LAB2/server/src/main/kotlin/aw2/g33/server/products/ProductController.kt
+++ LAB2/server/src/main/kotlin/aw2/g33/server/products/ProductController.kt
@@ -1,17 +1,21 @@
 package aw2.g33.server.products
 
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ProductController(private val productService: ProductService) {                                                                               //da json input genera json output
 
     @GetMapping("/API/products/")
+    @ResponseStatus(HttpStatus.OK)
     fun getAll():List<ProductDTO>{
         return productService.getAll()
     }
     @GetMapping("/API/products/{ean}")
+    @ResponseStatus(HttpStatus.OK)
     fun getProduct(@PathVariable ean:String):ProductDTO?{
         return productService.getProduct(ean)
     }
--- LAB2/server/src/main/kotlin/aw2/g33/server/profiles/ProfileController.kt
+++ LAB2/server/src/main/kotlin/aw2/g33/server/profiles/ProfileController.kt
@@ -2,21 +2,25 @@ package aw2.g33.server.profiles
 
 import aw2.g33.server.products.ProductDTO
 import org.apache.tomcat.util.json.JSONParser
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ProfileController(private val profileService: ProfileService) {
     @GetMapping("/API/profiles/{email}")
+    @ResponseStatus(HttpStatus.OK)
     fun getProfile(@PathVariable email:String):ProfileDTO?{
         return profileService.getProfileInfo(email)
     }
 
     @PostMapping("/API/profiles/")
+    @ResponseStatus(HttpStatus.CREATED)
     fun insertProfile(@RequestBody body:String): ProfileDTO {
         val parsed= JSONParser(body).parseObject();
         if(!parsed.containsKey("name") || !parsed.containsKey("email")){
@@ -28,6 +32,7 @@ class ProfileController(private val profileService: ProfileService) {
         return profile_to_add
     }
     @PutMapping("/API/profiles/{email}")
+    @ResponseStatus(HttpStatus.OK)
     fun updateProfile(@PathVariable email:String,@RequestBody body:String): ProfileDTO {
         val parsed= JSONParser(body).parseObject();
         if(!parsed.containsKey("name") || !parsed.containsKey("email")){
