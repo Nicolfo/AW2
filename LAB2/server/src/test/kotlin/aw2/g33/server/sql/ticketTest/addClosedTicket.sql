d54d6a01b4e3f1175035cbd85958c4bf4ac2d26d
Jacopo Spaccatrosi <jacopo.spaccatrosi@snapiot.com>
2023-05-04 13:39:10 +0200

    Update TicketTest and Create LogTicketsTests

---
+++ LAB2/server/src/test/kotlin/aw2/g33/server/sql/ticketTest/addClosedTicket.sql
@@ -0,0 +1,1 @@
+INSERT INTO public.ticket (ticket_id, description,priority,status,customer_email,worker_email) VALUES (gen_random_uuid(),'test',5,'CLOSED','jacopo@studenti.polito.it','mario.rossi@polito.it');
--- LAB2/server/src/test/kotlin/aw2/g33/server/sql/ticketTest/addProfile.sql
+++ LAB2/server/src/test/kotlin/aw2/g33/server/sql/ticketTest/addProfile.sql
@@ -1,1 +1,3 @@
-INSERT INTO public.profiles(email, name) VALUES ('jacopo@studenti.polito.it','Jacopo Spaccatrosi')
+INSERT INTO public.profiles(email, name) VALUES ('mario.rossi@polito.it','Mario Rossi');
+INSERT INTO public.profiles(email, name) VALUES ('jacopo@studenti.polito.it','Jacopo Spaccatrosi');
+
