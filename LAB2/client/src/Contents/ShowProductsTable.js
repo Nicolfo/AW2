f3d17c3bb8324005063a2f86dd00572f99d17ff4
Nicolfo <nicolfo@hotmail.it>
2023-12-11 17:53:37 +0100

    added index on Product list

--- LAB2/client/src/Contents/ShowProductsTable.js
+++ LAB2/client/src/Contents/ShowProductsTable.js
@@ -1,9 +1,12 @@
 import Table from 'react-bootstrap/Table';
+import Button from "react-bootstrap/Button";
+import {useState} from "react";
 
-function ShowProductsTable(props){
-    if(props.listOfProducts===undefined)
-        return(<h1>Loading Products...</h1>)
-    let rows=props.listOfProducts.map(row=>{
+function ShowProductsTable(props) {
+    let [index, setIndex] = useState(0);
+    if (props.listOfProducts === undefined)
+        return (<h1>Loading Products...</h1>)
+    let rows = props.listOfProducts.slice(0 + index * 10, 10 + index * 10).map(row => {
         return <tr key={row.productId}>
             <td>{row.productId}</td>
             <td>{row.name}</td>
@@ -12,7 +15,7 @@ function ShowProductsTable(props){
 
 
     })
-    return <Table striped bordered hover>
+    return <><Table striped bordered hover>
         <thead>
         <tr>
             <th>ProductId</th>
@@ -24,6 +27,27 @@ function ShowProductsTable(props){
         {rows}
         </tbody>
     </Table>
+        <div style={{position:"fixed",bottom:"10px"}}>
+            <Button disabled={index === 0} onClick={() => {
+                if (index > 0)
+                    setIndex((old) => {
+                        return old - 1;
+                    });
+            }
+            }>Prev</Button>
+            <Button disabled={index === Math.ceil(props.listOfProducts.length / 10) - 1} onClick={() => {
+
+                if (index < (props.listOfProducts.length / 10 - 1))
+                    setIndex((old) => {
+                        return old + 1;
+                    });
+            }
+            }>Next</Button>
+            Page {index + 1} of {Math.ceil(props.listOfProducts.length / 10)}
+
+        </div>
+    </>
+
 }
 
 export default ShowProductsTable;
