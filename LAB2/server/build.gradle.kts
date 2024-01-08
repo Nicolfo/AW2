f98c46bf24056021d31f2f416dafd8bff61558fa
Nicolfo <nicolfo@hotmail.it>
2024-01-08 15:16:02 +0100

    fixed a bug

--- LAB2/server/build.gradle.kts
+++ LAB2/server/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.0.5"
     id("io.spring.dependency-management") version "1.1.0"
-    id ("com.google.cloud.tools.jib") version "3.3.1"
+    id ("com.google.cloud.tools.jib") version "3.4.0"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
     kotlin("plugin.jpa") version "1.7.22"
